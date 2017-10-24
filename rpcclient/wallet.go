6bf4f8d6821b036fa6d549c9ed8a9fad1142ab6d
Ugwueze Michael <ugwueze.michael@gmail.com>
2017-10-24 12:07:21 +0100

    Passed PurchaseTicketsSingleTransaction to purchaseTickets function

--- rpcclient/wallet.go
+++ rpcclient/wallet.go
@@ -761,7 +761,7 @@ func (r FuturePurchaseTicketResult) Receive() ([]*chainhash.Hash, error) {
 func (c *Client) PurchaseTicketAsync(fromAccount string,
 	spendLimit dcrutil.Amount, minConf *int, ticketAddress dcrutil.Address,
 	numTickets *int, poolAddress dcrutil.Address, poolFees *dcrutil.Amount,
-	expiry *int, noSplitTransaction *bool) FuturePurchaseTicketResult {
+	expiry *int, noSplitTransaction *bool, purchaseTicketsSingleTransaction *bool) FuturePurchaseTicketResult {
 	// An empty string is used to keep the sendCmd
 	// passing of the command from accidentally
 	// removing certain fields. We fill in the
@@ -798,9 +798,19 @@ func (c *Client) PurchaseTicketAsync(fromAccount string,
 		expiryVal = *expiry
 	}
 
+	noSplitTx := false
+	if noSplitTransaction != nil {
+		noSplitTx = *noSplitTransaction
+	}
+
+	purchaseTicketsSingleTx := false
+	if purchaseTicketsSingleTransaction != nil {
+		purchaseTicketsSingleTx = *purchaseTicketsSingleTransaction
+	}
+
 	cmd := dcrjson.NewPurchaseTicketCmd(fromAccount, spendLimit.ToCoin(),
 		&minConfVal, &ticketAddrStr, &numTicketsVal, &poolAddrStr,
-		&poolFeesFloat, &expiryVal, nil, noSplitTransaction)
+		&poolFeesFloat, &expiryVal, nil, &noSplitTx, &purchaseTicketsSingleTx)
 
 	return c.sendCmd(cmd)
 }
@@ -810,10 +820,10 @@ func (c *Client) PurchaseTicketAsync(fromAccount string,
 func (c *Client) PurchaseTicket(fromAccount string,
 	spendLimit dcrutil.Amount, minConf *int, ticketAddress dcrutil.Address,
 	numTickets *int, poolAddress dcrutil.Address, poolFees *dcrutil.Amount,
-	expiry *int, noSplitTransaction *bool) ([]*chainhash.Hash, error) {
+	expiry *int, noSplitTransaction *bool, purchaseTicketsSingleTransaction *bool) ([]*chainhash.Hash, error) {
 
 	return c.PurchaseTicketAsync(fromAccount, spendLimit, minConf, ticketAddress,
-		numTickets, poolAddress, poolFees, expiry, noSplitTransaction).Receive()
+		numTickets, poolAddress, poolFees, expiry, noSplitTransaction, purchaseTicketsSingleTransaction).Receive()
 }
 
 // SStx generation RPC call handling
